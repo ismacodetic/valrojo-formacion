b839a6aef9ede69e9a5e6409110cf620af974f19
Ismael Maestre Carracedo <ismacodetic@gmail.com>
2026-03-22 03:49:08 +0100

    FIX BASE PATH

--- next.config.js
+++ next.config.js
@@ -1,16 +1,11 @@
 /** @type {import('next').NextConfig} */
-const isProduction = process.env.NODE_ENV === 'production'
-
 const nextConfig = {
   reactStrictMode: true,
   output: 'export',
   images: {
     unoptimized: true,
   },
-  ...(isProduction && {
-    basePath: '/valrojo-formacion',
-    assetPrefix: '/valrojo-formacion/',
-  }),
+  trailingSlash: true,
 }
 
 module.exports = nextConfig
