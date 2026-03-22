c05a4c2c63e35d9104cfaed7412b9df677e11280
Ismael Maestre Carracedo <ismacodetic@gmail.com>
2026-03-22 03:05:54 +0100

    Fixes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,11 @@
-import type { Metadata } from 'next'
+import type { Metadata, Viewport } from 'next'
 import './globals.css'
+
+export const viewport: Viewport = {
+  width: 'device-width',
+  initialScale: 1.0,
+  maximumScale: 5.0,
+}
 
 export const metadata: Metadata = {
   title: 'Valrojo Formación - Academia de Formación Digital',
