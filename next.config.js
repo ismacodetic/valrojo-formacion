68744e4a947595927e5219d2c5b4331cc6963e7d
Ismael Maestre Carracedo <ismacodetic@gmail.com>
2026-03-22 03:51:21 +0100

    Fix

--- next.config.js
+++ next.config.js
@@ -2,6 +2,8 @@
 const nextConfig = {
   reactStrictMode: true,
   output: 'export',
+  basePath: '/valrojo-formacion',
+  assetPrefix: '/valrojo-formacion/',
   images: {
     unoptimized: true,
   },
